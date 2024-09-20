ce866fd63a0b63a0330a598f324b8373680ec58a
AxelF0 <j7s3l1@gmail.com>
2024-09-20 18:04:33 -0400

    acv

---
+++ script.js
@@ -0,0 +1,8 @@
+pannellum.viewer('panorama', {
+    "type": "equirectangular",
+    "panorama": "images/360.jpeg",  /* Asegúrate de que esta imagen exista en la ruta correcta */
+    "autoLoad": true,
+    "autoRotate": -2,
+    "mouseZoom": false,
+    "draggable": false
+});
